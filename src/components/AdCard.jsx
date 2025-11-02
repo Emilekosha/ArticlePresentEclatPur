8c1cf05fe987aa31c2a9136fb37b3d9fe9263ea7
emilekosha <emilezolagarvane@gmail.com>
2025-11-02 13:57:18 +0000

    Initial commit

---
+++ src/components/AdCard.jsx
@@ -0,0 +1,27 @@
+import PubImg from '../assets/pub.jpg';
+
+export default function AdCard() {
+  return (
+    <a
+      href="https://emilekosha.github.io/LePerroquet_1/actualites/sponsors.html"
+      
+      rel="noopener noreferrer"
+      className="block bg-gray-100 rounded-lg overflow-hidden  max-w-md mx-auto "
+    >
+      <img
+        src={PubImg}
+        alt="Publicité robot éclat pur"
+        className="w-full h-64 object-cover"
+      />
+      <div className="p-4">
+        <p className="text-sm text-gray-500 font-semibold mb-2">
+          Présenté par <span className="text-gray-500 font-bold">ÉclatPur</span>
+        </p>
+        <p className="text-lg font-semibold text-gray-800 leading-snug">
+          L'Art de sublimer la brillance et<br />
+          la pureté de votre quotidien !
+        </p>
+      </div>
+    </a>
+  );
+}
